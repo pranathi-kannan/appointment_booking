ed0b17769657dc8adcbae18881737db637d82124
Pranathi_18 <136623905+pranathi-kannan@users.noreply.github.com>
2025-04-24 21:50:31 +0530

    Delete appointment_booking directory

--- appointment_booking/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_unset(); // Clear all session variables
-session_destroy(); // Destroy the session
-
-// Redirect to the login page after logout
-header("Location: login_admin.php");
-exit();
